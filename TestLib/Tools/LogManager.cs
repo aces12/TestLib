0129c5562a27059e719589cc20c478d78acf6869
kim_won <kim_won@ESG102PC109>
2017-08-21 10:40:25 +0900

    Add project files.

---
+++ TestLib/Tools/LogManager.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TestLib.Tools
+{
+    public class LogManager
+    {
+        //로그 파일 형식 : .\LogPath\2017\08\20170818.txt
+        public enum LogType { Daily, Monthly }
+
+        private string _path;
+        #region Constructor
+        public LogManager(string path, LogType logType, string prefix, string postfix)
+        {
+            _path = path;
+            _SetLogPath(logType, prefix, postfix);
+        }
+
+        public LogManager(string prefix, string postfix)
+            :this(Path.Combine(Application.Root, "Log"), LogType.Daily, prefix, postfix)
+        {
+
+        }
+        public LogManager()
+            : this(Path.Combine(Application.Root, "Log"), LogType.Daily, null, null)
+        {
+        }
+        #endregion
+
+        #region Method
+        private void _SetLogPath(LogType logType, string prefix, string postfix)
+        {
+            string path = String.Empty;
+            string name = String.Empty;
+
+            switch(logType)
+            {
+                case LogType.Daily:
+                    path = String.Format(@"{0}\{1}\", DateTime.Now.Year, DateTime.Now.ToString("MM"));
+                    name = DateTime.Now.ToString("yyyyMMdd");
+                    break;
+                case LogType.Monthly:
+                    path = String.Format(@"{0}\", DateTime.Now.Year);
+                    name = DateTime.Now.ToString("yyyyMM");
+                    break;
+            }
+
+            _path = Path.Combine(_path, path);
+            
+            if(!Directory.Exists(_path))
+            {
+                Directory.CreateDirectory(_path);
+            }
+
+            if (!String.IsNullOrEmpty(prefix))
+                name = prefix + name;
+            if (!String.IsNullOrEmpty(postfix))
+                name = name + postfix;
+            
+            _path = Path.Combine(_path, name);
+        }
+
+        public void Write(string data)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(_path, true))
+                {
+                    writer.Write(data);
+                }
+            }
+            catch (Exception ex)
+            { }
+        }
+        public void WriteLine(string data)
+        {
+            try
+            {
+                //FileStream fs = new FileStream(_path, FileMode.Create);
+                //using (StreamWriter writer = new StreamWriter(fs))
+                using (StreamWriter writer = new StreamWriter(_path, true))
+                {
+
+                    writer.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss\t") + data);
+                }
+                //fs.Close();
+            }
+            catch (Exception ex)
+            {
+                //using (StreamWriter writer = new StreamWriter(_path, true))
+                //{
+                //    writer.WriteLine(ex);
+                //}
+            }
+        }
+        //public void WriteConsole(string data)
+        //{
+        //    Write(data);
+        //    Console.Write(data);
+        //}
+        #endregion
+    }
+}
---
+++ TestLibTest/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using TestLib.Tools;
+using TestLib.Extensions;
+
+namespace TestLibTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Application.Root);
+            //Console.ReadLine();
+            //LogManager log = new LogManager(null, "_TestLibTest");
+
+            //log.WriteLine("[Begin Processing]------");
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    log.WriteLine("Processing: " + i);
+            //    // Do
+            //    Thread.Sleep(500);
+            //    log.WriteLine("Done: " + i);
+            //}
+            //log.WriteLine("[End Processing]------");
+
+            LogManager log = new LogManager();
+
+            //log.Write("test");
+            //log.WriteConsole("test");
+
+            //string temp = "1";
+            //string temp = "test";
+            string temp = "2017-06-01 11:20:33.33344564754578678968968";
+            //int temp = 0;
+            
+            Console.WriteLine(":IsNumeric : " + temp.IsNumeric());
+            Console.WriteLine(":IsDatetime : " + temp.IsDateTime());
+            
+            Console.ReadLine();
+
+
+        }
+       
+    }
+    //public static class ExtensionTest
+    //{
+    //    public static void WriteConsole(this LogManager log, string data)
+    //    {
+    //        log.Write(data);
+    //        Console.Write(data);
+    //        Console.ReadLine();
+    //    }
+
+    //}
+}
